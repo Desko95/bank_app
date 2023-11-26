e155da81631aa34a7f51036c5ff803f3b2f1653b
Desko <simonedescode@gmail.com>
2023-11-26 20:10:21 +0000

    created reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import styles from './style';
+import styles from "./style";
+//import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./";
+
+import Navbar from './components/Navbar';
+import Billing from "./components/Billing";
+import CardDeal from "./components/CardDeal";
+import Business from "./components/Business";
+import Clients from "./components/Clients";
+import CTA from "./components/CTA";
+import Stats from "./components/Stats";
+import Footer from "./components/Footer";
+import Testimonials from "./components/Testimonials";
+import Hero from "./components/Hero";
 
 const App = () => (
-  <div className='bg-primary w-full overflow-hidden'>
-    <div className={`${styles.paddingX}`}>
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
 
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
     </div>
   </div>
-  
 );
 
-export default App
+export default App;
