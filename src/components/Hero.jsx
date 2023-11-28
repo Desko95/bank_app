8aab8051d756e1ab0be29536c0b6de6bcf7eb24c
Desko <simonedescode@gmail.com>
2023-11-28 12:35:41 +0000

    index errors fix + herosection

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,18 +16,39 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
-            The Next  <br className="sm-block hidden" /> {" "}
-            <span className="text-gradient">Generation</span> {" "}
-            Payment Method.
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
-
           </div>
-
         </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Payment Method.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
       </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        {/* gradient start */}
+        <div className="absolute z-[1] w-[40%] h-[35%] top-0 pink__gradient opacity-50" />
+        <div className="absolute z-[2] w-[80%] h-[80%] rounded-full white__gradient bottom-40 opacity-80" />
+        <div className="absolute z-[3] w-[50%] h-[50%] right-20 bottom-20 blue__gradient opacity-80" />
+        {/* gradient end */}
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+
     </section>
   );
 };
